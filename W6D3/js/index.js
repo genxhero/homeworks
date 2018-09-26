031ddcc33d494aa6bc97d1d1bda3d46f688d204f
Aaron Goddard <aaronbear@gmail.com>
2018-09-25 20:08:48 -0700

    complete Ajax request

--- W6D3/js/index.js
+++ W6D3/js/index.js
@@ -1,5 +1,15 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
-
+$.ajax({
+type: "GET",
+url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
+success(stuff) {
+    console.log(stuff);
+    console.log("I'm making a note here, huge success.")
+  },
+  error() {
+    console.error("No weather for you!!!!!");
+  },
+});
 // Add another console log here, outside your AJAX request
