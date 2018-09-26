668422d4e78eea1bcc1a3e588ab63e1347429dcc
Aaron Goddard <aaronbear@gmail.com>
2018-09-26 13:12:45 -0700

    Do most of the homework

---
+++ W6D4/vdm/script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // toggling restaurants
+
+  const toggleLi = (e) => {
+    const li = e.target;
+    if (li.className === "visited") {
+      li.className = "";
+    } else {
+      li.className = "visited";
+    }
+  };
+
+  document.querySelectorAll("#restaurants li").forEach((li) => {
+    li.addEventListener("click", toggleLi);
+  });
+
+
+  // adding SF places as list items
+
+  const addToList = (element) => {
+     element.preventDefault();
+     const input = document.querySelector(".favorite-input");
+     const favorite = input.value;
+     input.value = "";
+     const newItem = document.createElement("li");
+     newItem.textContent = favorite;
+
+     const places = document.getElementById("sf-places");
+     places.appendChild(newItem);
+   };
+
+   const favoriteButton = document.querySelector(".favorite-submit");
+   favoriteButton.addEventListener("click", addToList);
+
+
+  // adding new photos
+
+const newPhoto = (photo) => {
+  const photoInfo = document.querySelector(".photo-url-input");
+  const newPic = photoInfo.value;
+  photoInfo.value = "";
+  const newItem = document.createElement("li img");
+  const photos = document.getElementById("photos")
+  newItem.i
+};
+
+const photoButton = document.querySelector(".photo-url-submit")
+photoButton.addEventListener("click", newPhoto);
