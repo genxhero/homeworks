4c045f7ed36dc0d87865b1f6e61f3ab8f3b311fe
Aaron Goddard <aaronbear@gmail.com>
2018-09-27 13:29:14 -0700

    finish getting started

--- W6D5/npm/webpack.config.js
+++ W6D5/npm/webpack.config.js
@@ -1,6 +1,26 @@
 module.exports = {
   entry: './entry.jsx',
-}
+  output: {
+     filename: './congrats.jsx',
+   },
+   rules: [
+     {
+       test: [/\.jsx?$/],
+       exclude: /(node_modules)/,
+       use: {
+         loader: 'babel-loader',
+         query: {
+           presets: ['@babel/env', '@babel/react']
+         }
+       },
+     }
+   ]
+ },
+ devtool: 'source-map',
+ resolve: {
+   extensions: ['.js', '.jsx', '*']
+ }
+};
 
 
 // Create webpack.config.js and assign module.exports to the Webpack
