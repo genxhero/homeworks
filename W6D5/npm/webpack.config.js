1e371fe53844fbc3e8e1c1d5e4f8545453222c7e
Aaron Goddard <aaronbear@gmail.com>
2018-09-27 13:21:58 -0700

    Attempt to start react project

---
+++ W6D5/npm/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './entry.jsx',
+}
+
+
+// Create webpack.config.js and assign module.exports to the Webpack
+//configuration object. Make sure to do the following:
+//
+// Set your entry and output files.
+// Configure Babel transpilation of React and ES6 syntax
+// Add a devtool
+// Ensure that .js and .jsx files resolve automatically
+// In the generated package.json:
+//
+// Add a webpack script for webpack to your package.json ("webpack": "webpack --mode=development --watch")
